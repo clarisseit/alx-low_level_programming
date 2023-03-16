cdc91f9b8eab6fab06302933426d9bd3dfa9b71f
clarisseit <Clarisse21445@mail.com>
2023-03-16 08:06:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: @ (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (@);
+}
