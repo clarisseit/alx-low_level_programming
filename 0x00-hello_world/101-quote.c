47ccbf09f277133e67dc14c8030a0f01cf7c3e7b
clarisseit <Clarisse21445@mail.com>
2023-03-16 15:12:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  main - A C program that prints a lineto the tandard error
- *  Return: 0 (Success)
+ *  main - A C program that prints a line to the standard error
+ *  Return: 1 (Success)
  */
 int main(void)
 {
-        fput("and that piece of art is useful\* - Dora Korpar, 2015-10-19\n",stdout);
-        return (1);
+fput("and that piece of art is useful\* - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
 
