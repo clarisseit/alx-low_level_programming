779ada8a7c53180a1c78ffcd3e9cc82b18808a20
clarisseit <Clarisse21445@mail.com>
2023-05-10 10:49:37 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN.N
+#define MAIN.N
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,8 +12,4 @@ int is_prime_number(int n);
 int getNumPrim(int num, int i);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-
-
-
-
+#endif
